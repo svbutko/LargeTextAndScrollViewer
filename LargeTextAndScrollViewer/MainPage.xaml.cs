46f7ec3d7bc8f7c1d13547c6cb985b582e2af40d
Sergei Butko <svbutko@gmail.com>
2017-03-13 23:59:38 +0200

    Add project files.

---
+++ LargeTextAndScrollViewer/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace LargeTextAndScrollViewer
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var bounds = Window.Current.Bounds;
+            InnerStackPanel.Width = bounds.Width;
+        }
+    }
+}
